1a121ff0949b98b6ab569b7452b373fe982c8d78
b3b00 <olivier.duhart@gmail.com>
2024-05-07 09:17:14 +0200

    remove dead code

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -32,40 +32,9 @@ public static class Program
     public static void Main(string[] args)
     {
         TestDate();
-        //TestScript();
     }
 
-    private static void TestScript()
-    {
-        var grammar = File.ReadAllText("./script.grammar.txt");
-        var builder = new ParserBuilder();
-        var model = builder.CompileModel(grammar, "MinimalParser");
-        if (model.IsOk)
-        {
-            Console.WriteLine("model is OK");
-            var source = File.ReadAllText("./script.txt");
-            var dot = builder.Getz(grammar, source, "ScriptParser", new List<(string format, SyntaxTreeProcessor processor)>() {("DOT",ParserBuilder.SyntaxTreeToDotGraph)});
-            if (dot.IsOk)
-            {
-                File.WriteAllText("c:/tmp/script.dot",dot.Value.FirstOrDefault(x => x.format == "DOT").content);
-                Console.Write("script is ok");
-            }
-            else
-            {
-                foreach (var error in dot.Error)
-                {
-                    Console.WriteLine(error);
-                }
-            }
-        }
-        else
-        {
-            foreach (var error in model.Error)
-            {
-                Console.WriteLine(error);
-            }
-        }
-    }
+   
 
     private static void TestDate()
     {

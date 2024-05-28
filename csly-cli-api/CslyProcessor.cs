77f56ee46dd25ea6f716b665e3c51bb66e5110cd
b3b00 <olivier.duhart@gmail.com>
2024-05-28 13:54:51 +0200

    csly cli API : source generation

--- csly-cli-api/CslyProcessor.cs
+++ csly-cli-api/CslyProcessor.cs
@@ -1,18 +1,19 @@
-﻿using clsy.cli.builder.parser;
+﻿using clsy.cli.builder;
+using clsy.cli.builder.parser;
 
 namespace csly_cli_api;
 
 
 
-public class CliResult
+public class CliResult<T>
 {
     private bool _isOk;
 
     public bool IsOK => _isOk;
     
-    private string _result;
+    private T _result;
 
-    public string Result => _result;
+    public T Result => _result;
 
     private List<string> _errors;
 
@@ -24,16 +25,34 @@ public class CliResult
         _errors = errors;
     }
 
-    public CliResult(string result)
+    public CliResult(T result)
     {
         _isOk = true;
         _result = result;
     }
 }
 
+public class GeneratedSource
+{
+    
+    public string LexerName { get; set; }
+    public string Lexer { get; set; }
+
+    public string ParserName { get; set; }
+    public string Parser { get; set; }
+
+    public GeneratedSource(string lexerName, string lexer, string parserName, string parser)
+    {
+        LexerName = lexerName;
+        ParserName = parserName;
+        Lexer = lexer;
+        Parser = parser;
+    }
+}
+
 public class CslyProcessor
 {
-    public static CliResult GetDot(string grammar, string source)
+    public static CliResult<string> GetDot(string grammar, string source)
     {
         var builder = new ParserBuilder();
         var model = builder.CompileModel(grammar, "MinimalParser");
@@ -44,20 +63,20 @@ public class CslyProcessor
                     { ("DOT", (SyntaxTreeProcessor)ParserBuilder.SyntaxTreeToDotGraph) },model.Value.ParserModel.Root);
             if (r.IsError)
             {
-                return new CliResult(r.Error.Select(x => $"parse error : {x}").ToList());
+                return new CliResult<string>(r.Error.Select(x => $"parse error : {x}").ToList());
             }
             else
             {
-                return new CliResult(r.Value[0].content);
+                return new CliResult<string>(r.Value[0].content);
             }
         }
         else
         {
-            return new CliResult(model.Error.Select(x => $"grammar error : {x}").ToList());
+            return new CliResult<string>(model.Error.Select(x => $"grammar error : {x}").ToList());
         }
     }
-
-    public static CliResult GetJson(string grammar, string source)
+   
+    public static CliResult<string> GetJson(string grammar, string source)
     {
         var builder = new ParserBuilder();
         var model = builder.CompileModel(grammar, "MinimalParser");
@@ -68,16 +87,46 @@ public class CslyProcessor
                     { ("JSON", (SyntaxTreeProcessor)ParserBuilder.SyntaxTreeToDotGraph) });
             if (r.IsError)
             {
-                return new CliResult(r.Error.Select(x => $"parse error : {x}").ToList());
+                return new CliResult<string>(r.Error.Select(x => $"parse error : {x}").ToList());
             }
             else
             {
-                return new CliResult(r.Value[0].content);
+                return new CliResult<string>(r.Value[0].content);
             }
         }
         else
         {
-            return new CliResult(model.Error.Select(x => $"grammar error : {x}").ToList());
+            return new CliResult<string>(model.Error.Select(x => $"grammar error : {x}").ToList());
+        }
+    }
+
+    public static CliResult<GeneratedSource> GenerateParser(string grammar, string nameSpace, string outputType)
+    {
+        var builder = new ParserBuilder();
+        var model = builder.CompileModel(grammar, "MinimalParser");
+        if (model.IsOk)
+        {
+            string lexerSource = "";
+            string parserSource = "";
+            
+            var lexerModel = model.Value.LexerModel;
+            var parserModel = model.Value.ParserModel;
+        
+            Console.WriteLine("Model compilation succeeded.");
+
+            var lexerGenerator = new LexerGenerator();
+            lexerSource = lexerGenerator.GenerateLexer(lexerModel, nameSpace);
+
+            var parserGenerator = new ParserGenerator();
+            parserSource = parserGenerator.GenerateParser(model.Value, nameSpace, outputType);
+            
+            
+            
+            return new CliResult<GeneratedSource>(new GeneratedSource(model.Value.LexerModel.Name, lexerSource, model.Value.ParserModel.Name, parserSource));
+        }
+        else
+        {
+            return new CliResult<GeneratedSource>(model.Error.Select(x => $"grammar error : {x}").ToList());
         }
     }
 }

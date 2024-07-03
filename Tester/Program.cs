7d3ee8c5745058794e190a484839e86cc57fcf1c
b3b00 <olivier.duhart@gmail.com>
2024-07-03 08:37:04 +0200

    CLI 2.2.8 API 1.4.1

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -198,7 +198,7 @@ parser HexaParser;
             File.WriteAllText("C:\\Users\\olduh\\dev\\csly-cli\\Generated\\SomeParser.cs",x.Result.Parser);
         }
 
-        var t = processor.GetDot(grammar, "6x12 6xAB 0xab 999");
+        var t = processor.GetDot(grammar, "6x12 6xAB 66x28 999");
         if (t.IsOK)
         {
             Console.WriteLine("parse ok");
